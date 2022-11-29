35d9afc8d4678dc69b5430904a50960a26b0c368
Tonya-Shyva <toshash69@gmail.com>
2022-11-29 12:27:58 +0200

    Update App.jsx
    initialContacts from json

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,16 +6,11 @@ import { Contacts } from 'components/ContactList/ContactList';
 import { AppContainer, Title } from './App.styled';
 import { Filter } from 'components/Filter/Filter';
 
-// import initialContacts from '../initialContacts.json';
+import initialContacts from '../initialContacts.json';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: initialContacts,
     filter: '',
   };
 
